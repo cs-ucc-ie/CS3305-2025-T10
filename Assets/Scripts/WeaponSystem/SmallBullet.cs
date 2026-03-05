30eeffee524d441a3162d1bca959c39d55ccc863
Ling chen <145187919+CHen12313@users.noreply.github.com>
2026-03-05 12:29:00 +0000

    minor fix on pallet (small bullet) logic

--- Assets/Scripts/WeaponSystem/SmallBullet.cs
+++ Assets/Scripts/WeaponSystem/SmallBullet.cs
@@ -4,25 +4,12 @@ public class SmallBullet : BulletFramework
 {   
     protected override void OnEnable()
     {
-    base.OnEnable();
 
-    Collider myCol = GetComponent<Collider>();
-    if (myCol == null) return;
-
-    SmallBullet[] others = FindObjectsOfType<SmallBullet>();
-
-    foreach (var other in others)
-    {
-        if (other == this) continue;
-
-        Collider otherCol = other.GetComponent<Collider>();
-        if (otherCol != null)
-            Physics.IgnoreCollision(myCol, otherCol, true);
-    }
     }
     protected override void OnHit(Collision collision)
     {
-       
+       if (collision.gameObject.CompareTag("Pallet")) return;
+
         var ai = collision.gameObject.GetComponent<EnemyAI>();
         if (ai == null) return;
 
