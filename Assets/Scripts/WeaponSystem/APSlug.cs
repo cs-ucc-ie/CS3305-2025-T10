38860563cfea709bd2c570070ee1e07b6401adda
Ling chen <145187919+CHen12313@users.noreply.github.com>
2026-03-05 12:19:34 +0000

    added knock back for AP Slug and fixed typo
    Added knock back for AP slug
    Fixed typo for HESlug:
    knockBackDir to knockBackDur for knock back duration

--- Assets/Scripts/WeaponSystem/APSlug.cs
+++ Assets/Scripts/WeaponSystem/APSlug.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class APSlug : BulletFramework
 {
+    [Header("Optional Enemy Knockback")]
+    [SerializeField] private float knockBackForce = 3f;
+    [SerializeField] private int knockBackDur = 1;
     protected override void OnHit(Collision collision)
     {
        
         var ai = collision.gameObject.GetComponent<EnemyAI>();
         if (ai == null) return;
 
-        
+        Vector3 center = transform.position;
+        ai.TakeDamage((int)damage);
+        ai.KnockBack(center, knockBackForce, knockBackDur);
         ai.TakeDamage((int)damage);
     }
 }
